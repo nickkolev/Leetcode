48b0ac79b40237b12bf35eec2d5060b7adf492e8
Nikola Kolev <nickkolev06@abv.bg>
2024-02-21 23:13:45 +0200

    2 more solutions for 1581 SQL task

--- 01.SQL/1581.CustomerWhoVisitedButDidNotMakeAnyTransactions.sql
+++ 01.SQL/1581.CustomerWhoVisitedButDidNotMakeAnyTransactions.sql
@@ -1,6 +1,24 @@
-SELECT v.customer_id, COUNT(v.visit_id) AS count_no_trans 
-from Visits v 
-LEFT JOIN Transactions t 
-ON v.visit_id = t.visit_id  
-WHERE t.amount IS NULL 
-GROUP BY v.customer_id; 
+
+/* LEFT JOIN */
+SELECT customer_id, COUNT(v.visit_id) as count_no_trans 
+FROM Visits v
+LEFT JOIN Transactions t ON v.visit_id = t.visit_id
+WHERE transaction_id IS NULL
+GROUP BY customer_id
+
+/* NOT IN */
+SELECT customer_id, COUNT(visit_id) as count_no_trans 
+FROM Visits
+WHERE visit_id NOT IN (
+	SELECT visit_id FROM Transactions
+	)
+GROUP BY customer_id
+
+/* NOT EXISTS */
+SELECT customer_id, COUNT(visit_id) as count_no_trans 
+FROM Visits v
+WHERE NOT EXISTS (
+	SELECT visit_id FROM Transactions t 
+	WHERE t.visit_id = v.visit_id
+	)
+GROUP BY customer_id
