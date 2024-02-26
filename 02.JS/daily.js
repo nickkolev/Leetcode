71f2dd4f73e5dfaf66fe7562e2ea1e90b5e5c882
Nikola Kolev <nickkolev06@abv.bg>
2024-02-26 21:25:22 +0200

    JS daily

--- 02.JS/daily.js
+++ 02.JS/daily.js
@@ -24,3 +24,10 @@ let findJudge = function(N, trust) {
 };
 
 findJudge(2, [[1,3],[2,3],[3,1]]);
+
+
+var isSameTree = function(p, q) {
+    if (!p && !q) return true;
+    if (!p || !q) return false;
+    return p.val === q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+};
